b20d49dd1f790b730c3cc6c53ad7b0f1bab03097
Akihiro Yamada <u.parr917@gmail.com>
2019-05-14 23:48:15 +0900

    first commit

---
+++ src/main/java/com/rakucari/aki/RakucariApiApplication.java
@@ -0,0 +1,13 @@
+package com.rakucari.aki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RakucariApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RakucariApiApplication.class, args);
+	}
+
+}
