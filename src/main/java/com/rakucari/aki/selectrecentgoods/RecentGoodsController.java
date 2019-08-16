81deacaa6af69a81350e3ffcaa668f6c61419f72
Akihiro Yamada <u.parr917@gmail.com>
2019-08-16 09:46:03 +0900

    最近追加された商品取得のURLを変更。

--- src/main/java/com/rakucari/aki/selectrecentgoods/RecentGoodsController.java
+++ src/main/java/com/rakucari/aki/selectrecentgoods/RecentGoodsController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
  *
  */
 
-@RequestMapping("latest-goods")
+@RequestMapping("recent-goods")
 @RestController
 @CrossOrigin
 @Scope("prototype")
