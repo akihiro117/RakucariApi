0c9133022c1ffbf31fdfa4c4aa552563d8fddbae
Akihiro Yamada <u.parr917@gmail.com>
2019-08-18 14:34:01 +0900

    指定された固定のIDの商品の詳細情報をDBから取得してクライアントに返す処理を実装。

--- src/main/java/com/rakucari/aki/goods/Goods.java
+++ src/main/java/com/rakucari/aki/goods/Goods.java
@@ -12,11 +12,29 @@ public class Goods {
     /** 商品名。*/
     private String goodsName;
 
-    /** 商品詳細ページのURL。 */
-    private String goodsDetailUrl;
-
     /** 商品画像のURL。 */
     private String imgUrl;
+
+    /** カテゴリー(大項目)。 */
+    private String majorCategory;
+
+    /** 商品の状態。 */
+    private String useStatus;
+
+    /** 配送料負担。 */
+    private String feeResponsibility;
+
+    /** 配送元地域。 */
+    private String srcRegion;
+
+    /** 発送日の目安。 */
+    private String shippingDate;
+
+    /** 価格。 */
+    private int price;
+
+    /** 商品説明。 */
+    private String description;
 
     public String getId() {
         return id;
@@ -34,20 +52,68 @@ public class Goods {
         this.goodsName = goodsName;
     }
 
-    public String getGoodsDetailUrl() {
-        return goodsDetailUrl;
-    }
-
-    public void setGoodsDetailUrl(String goodsDetailUrl) {
-        this.goodsDetailUrl = goodsDetailUrl;
-    }
-
     public String getImgUrl() {
         return imgUrl;
     }
 
     public void setImgUrl(String imgUrl) {
         this.imgUrl = imgUrl;
+    }
+
+    public String getMajorCategory() {
+        return majorCategory;
+    }
+
+    public void setMajorCategory(String majorCategory) {
+        this.majorCategory = majorCategory;
+    }
+
+    public String getUseStatus() {
+        return useStatus;
+    }
+
+    public void setUseStatus(String useStatus) {
+        this.useStatus = useStatus;
+    }
+
+    public String getFeeResponsibility() {
+        return feeResponsibility;
+    }
+
+    public void setFeeResponsibility(String feeResponsibility) {
+        this.feeResponsibility = feeResponsibility;
+    }
+
+    public String getSrcRegion() {
+        return srcRegion;
+    }
+
+    public void setSrcRegion(String srcRegion) {
+        this.srcRegion = srcRegion;
+    }
+
+    public String getShippingDate() {
+        return shippingDate;
+    }
+
+    public void setShippingDate(String shippingDate) {
+        this.shippingDate = shippingDate;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 
 }
